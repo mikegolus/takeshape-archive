f0a727e79e623dc8eb41acede8703f8401cc8ca9
Mike Golus <michaelgolus@gmail.com>
2019-03-26 16:08:35 -0400

    Updated 404 page to be the same as home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
 import Parallax from "parallax-js"
 import { graphql } from "gatsby"
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,6 +10,7 @@ class IndexPage extends React.Component {
   componentDidMount () {
     var img = new Image();
     img.src = "https://images.unsplash.com/photo-1487715433499-93acdc0bd7c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2614&q=80";
+    img.setAttribute("alt","Background Image");
     img.onload = () => {
       this.layer.appendChild(img);
       new Parallax(this.scene, {
@@ -23,7 +24,7 @@ class IndexPage extends React.Component {
     const { data } = this.props;
     return (
       <Layout>
-        <SEO title="TAKE SHAPE" image="/img/facebook-share.png" imageWidth="1200" imageHeight="630" />
+        <SEO image="/img/facebook-share.png" imageWidth="1200" imageHeight="630" />
         <div className="background">
           <div className="scene" ref={(el) => this.scene = el}>
             <div className="layer" data-depth="0.4" ref={(el) => this.layer = el}></div>
@@ -39,10 +40,10 @@ class IndexPage extends React.Component {
         </div>
         <div className="text-wrapper">
           <div className="text">
-            <p className="larger">{data.allMarkdownRemark.edges[0].node.frontmatter.heading}</p>
+            <h1 className="larger">{data.allMarkdownRemark.edges[0].node.frontmatter.heading}</h1>
             <p className="smaller">
               <a href={data.allMarkdownRemark.edges[0].node.frontmatter.projectLink} target="_blank" rel="noopener noreferrer">{data.allMarkdownRemark.edges[0].node.frontmatter.projectButtonText}</a>
-              <Link to="/contact">Send us a message</Link>
+              <AniLink fade duration={0.2} to="/contact">Send us a message</AniLink>
             </p>
           </div>
         </div>
